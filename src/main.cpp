e9420591d2bd024d84221a522f60b0ce676ee832
Majki987 <163041567+Majki987@users.noreply.github.com>
2025-11-03 15:27:51 +0000

    zad1.2

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,34 @@
 //imie i nazwisko
-
+#include <fstream>
 #include <iostream>
+using namespace std;
+
+
+
+string zad3 ="../zalaczniki-2025/symbole.txt";
+bool czy_palindrom(string tekst){
+    int ile_znakow = tekst.length();
+    for(int i=0; i< ile_znakow/2 ; i++){
+        if(tekst[i] != tekst[ile_znakow-i-1])
+            return false;
+    }
+    return true;
+
+}
+void Zadanie3_1(){
+    ifstream plik(zad3);
+    if(!plik.is_open()){
+        cerr << "Nie mozna otworzyc pliku"<< endl;
+    }
+    string linia ;
+    while(plik >> linia){
+        if(czy_palindrom(linia)){
+            cout << linia <<endl;
+        }
+    }
+    plik.close();
+}
 int main() {
-    std::cout << "Hello, Matura 2025!\n";
+    Zadanie3_1();
     return 0;
 }
